68bd978754630c871171e9be00d72db23c42f303
Sami Saada <sami.saada@gmail.com>
2019-10-08 18:50:27 +0300

    Register monitor and robot in brain

--- brain/main.py
+++ brain/main.py
@@ -6,8 +6,32 @@ import tornado.ioloop
 from tornado.log import enable_pretty_logging
 enable_pretty_logging()
 
+class Brain:
+    def __init__(self):
+        self._monitor = None
+        self._robot = None
+
+    @property
+    def monitor(self):
+        return self._monitor
+    @monitor.setter
+    def monitor(self, monitor):
+        self._monitor = monitor
+        logging.info("new monitor set")
+
+    @property
+    def robot(self):
+        return self._robot
+    @robot.setter
+    def robot(self, robot):
+        self._robot = robot
+        logging.info("new robot set")
 
 class CommonBrainHandler(websocket.WebSocketHandler):
+
+    def initialize(self, brain):
+        self.brain = brain
+
     def check_origin(self, origin):
         return True
 
@@ -18,18 +42,28 @@ class CommonBrainHandler(websocket.WebSocketHandler):
         logging.info("Websocket closed")
 
 class MonitorHandler(CommonBrainHandler):
+    def open(self):
+        self.brain.monitor = self
+
     def on_message(self, message):
         self.write_message(u"Monitor said: %s" % message)
 
 class RobotHandler(CommonBrainHandler):
+    def open(self):
+        self.brain.robot = self
+
     def on_message(self, message):
         self.write_message(u"Robot said: %s" % message)
 
-application = tornado.web.Application([
-    (r"/monitor", MonitorHandler),
-    (r"/robot", RobotHandler),
-    ])
 
 if __name__ == "__main__":
+    brain = Brain()
+
+    brain_operating = tornado.ioloop.PeriodicCallback(brain.operate, 1000)
+    application = tornado.web.Application([
+        (r"/monitor", MonitorHandler, dict(brain=brain)),
+        (r"/robot", RobotHandler, dict(brain=brain)),
+        ])
     application.listen(9000)
+
     tornado.ioloop.IOLoop.current().start()
